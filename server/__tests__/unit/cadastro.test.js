822cb016bf8d380649636f615845b9b84da91d15
guilherme <guilhermedomith@gmail.com>
2020-01-30 13:43:12 -0200

    test: register products api

--- server/__tests__/unit/cadastro.test.js
+++ server/__tests__/unit/cadastro.test.js
@@ -1,11 +1,48 @@
-const app = await require('../../src/app')
+const app = require('../../src/app.js')
+const request = require('supertest')
+const truncate = require('../utils/truncate.js')
 
 describe('cadastro', () => {
-    it('should return sum operation', async () => {
-        var a = 6
-        var b = 4
-
-        var sum = a + b
-        expect(sum).toBe(10)
+    beforeAll(async () => {
+        await truncate();
     })
+
+    afterAll(async () => {
+        await truncate();
+    })
+
+    it('should register a product', async () => {
+        let produto = {
+            descricao: 'Embalagem 1',
+            data_update: '20200127',
+            quantidade: 1000
+        }
+
+        const response = await request(app)
+            .post('/produto')
+            .send(produto)
+        
+        expect(response.status).toBe(200)
+    })
+
+    it('should not register a product with the same description', async () => {
+        let produto = {
+            descricao: 'Embalagem 1',
+            data_update: '20200126',
+            quantidade: 2000
+        }
+
+        const response = await request(app)
+            .post('/produto')
+            .send(produto)
+        
+        expect(response.status).toBe(400)
+    })
+
+    it('should list all products of database', async () =>{
+        const response = await request(app)
+            .get('/produto')
+
+        expect(response.body.length).toBe(1)
+    }) 
 })
